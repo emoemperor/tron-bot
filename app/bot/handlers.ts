59456aad81f4348d7ec4955f7a306f0092ad57f5
emoemperor <github@emocat.icu>
2024-08-20 17:39:30 +0800

    修正时区

--- app/bot/handlers.ts
+++ app/bot/handlers.ts
@@ -1,5 +1,10 @@
 import { IContext, SessionAction } from ".";
 import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.tz.setDefault("Asia/Shanghai");
 import {
   CallbackQueryContext,
   CommandContext,
